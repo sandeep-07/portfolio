851c1e40d152dd3aa24d8f16abf5f4e7eb82c3c6
sandeep-07 <sandeepisthename007@gmail.com>
2024-10-25 22:10:42 +0530

    fix build

--- lib/modules/Work/WorkSliderButtons.tsx
+++ lib/modules/Work/WorkSliderButtons.tsx
@@ -3,19 +3,18 @@
 import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }: {
+const WorkSliderBtns = ({ containerStyles, btnStyles }: {
     containerStyles: string;
     btnStyles: string;
-    iconsStyles: string;
 }) => {
   const swiper = useSwiper();
   return (
     <div className={containerStyles}>
       <button className={btnStyles} onClick={() => swiper.slidePrev()}>
-        <PiCaretLeftBold className={iconsStyles} />
+        <PiCaretLeftBold  />
       </button>
       <button className={btnStyles} onClick={() => swiper.slideNext()}>
-        <PiCaretRightBold className={iconsStyles} />
+        <PiCaretRightBold  />
       </button>
     </div>
   );
